6cee13909b2dcdf78e6c51b18ee769997536d5af
msheepy <monelson@ualberta.ca>
2025-09-27 15:23:21 -0600

    Added third fragment. Got navigation working. First page has basic functionality implemented.

---
+++ monelson-EmotiLog/app/src/main/java/com/example/monelson_emotilog/FirstFragment.java
@@ -0,0 +1,68 @@
+package com.example.monelson_emotilog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.monelson_emotilog.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FirstFragment extends Fragment {
+    private ListView lv;
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        lv = (ListView) view.findViewById(R.id.emotion_list);
+        List<String> options = new ArrayList<String>();
+        options.add("\uD83D\uDE10");
+        options.add("\uD83D\uDE26");
+        options.add("\uD83D\uDE2C");
+        options.add("\uD83D\uDE27");
+        options.add("\uD83D\uDE28");
+        options.add("\uD83D\uDE2D");
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, options );
+
+        lv.setAdapter(arrayAdapter);
+
+        binding.changeButton.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+        binding.dayButton.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_ThirdFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
